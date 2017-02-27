90c376ae8cbd065407b72e746a5efc16e554f1f2
Sara Trempe <88.trempe.sara@gmail.com>
2017-02-27 16:02:13 -0500

    Use Etsy Data To Anwser Questions

--- esty.js
+++ esty.js
@@ -1,14 +1,13 @@
 console.log('1.');
 // The average price is
-var itemPrice = [];
 var itemPrice = items.map(getItemPrice);
 function getItemPrice(item) {
     return item.price;
 }
-var total = itemPrice.reduce(function(prev,next) {
+var total = itemPrice.reduce(function(prev, next) {
     return prev + next;
 }, 0);
-console.log(total/itemPrice.length);
+console.log(Math.ceil((total/itemPrice.length) * 100) /100);
 
 
 console.log('2.');
@@ -27,7 +26,7 @@ var euroItems = items.filter(function(item){
     return item.price && item.currency_code === "GBP"
 })
 euroItems.forEach(function(item){
-    console.log(item.title)
+    console.log(item.title + ' £' + item.price);
 })
 
 
@@ -43,16 +42,18 @@ woodItems.forEach(function(item) {
 console.log('5.');
 // items are made of eight or more materials
 var materialItems = items.filter(function(item){
-    return item.materials >= [8]
+    return item.materials.length >= 8
 })
 materialItems.forEach(function(item){
     console.log(item.title + 'has' + item.materials.length + 'materials:' + item.materials.join('\n'))
+
 })
 
 console.log('6.');
 // Calculate how many items were made by their sellers.
 var sellers = items.filter(function(item){
-    return item.who_made.includes('i_did')
+    return item.who_made === 'i_did';
+    // return item.who_made.includes('i_did')
 });
     console.log(sellers.length + ' items were made by their seller.')
 
